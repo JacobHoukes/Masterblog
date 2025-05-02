11f3ab274feff1191e7e5523ab9eadada89223a0
Jacob <jayhoukes@gmail.com>
2025-05-02 23:43:32 +0200

    blog post update functionality implemented

--- app.py
+++ app.py
@@ -52,5 +52,20 @@ def delete(post_id):
     return redirect(url_for('index'))
 
 
+@app.route('/update/<int:post_id>', methods=['GET', 'POST'])
+def update(post_id):
+    blog_posts = load_blog_posts()
+    post = next((post for post in blog_posts if post['id'] == post_id), None)
+    if post is None:
+        return "Post not found", 404
+    elif request.method == 'POST':
+        post['author'] = request.form.get('author')
+        post['title'] = request.form.get('title')
+        post['content'] = request.form.get('content')
+        save_blog_posts(blog_posts)
+        return redirect(url_for('index'))
+    return render_template('update.html', post=post)
+
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5002, debug=True)
