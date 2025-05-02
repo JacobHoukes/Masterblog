b46fbb5019bb548097f11ed1608b5b9c6f795217
Jacob <jayhoukes@gmail.com>
2025-05-02 22:55:32 +0200

    static and templates files and json added, app.py updated

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
+# Helper function to load blog posts from JSON file
+def load_blog_posts():
+    with open('blog_posts.json', 'r') as file:
+        return json.load(file)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
-
+def index():
+    blog_posts = load_blog_posts()
+    return render_template('index.html', posts=blog_posts)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5002, debug=True)
